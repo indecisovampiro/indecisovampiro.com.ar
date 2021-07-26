81f6cecc4a4919d5eb7cc24a231e24ceb98c2f60
Sobrino Julian <sobrinojulian@pm.me>
2021-07-26 19:52:42 -0300

    npx tailwind

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,4 @@
 module.exports = function (config) {
-  // Watch!
-  config.addWatchTarget('_site/tailwind.css')
-
   // Copy!
   // Favicon
   config.addPassthroughCopy({ 'static/favicon.ico': 'favicon.ico' })
@@ -13,7 +10,6 @@ module.exports = function (config) {
   config.addPassthroughCopy({
     'node_modules/alpinejs/dist/alpine.js': 'alpine.js',
     'node_modules/fitty/dist/fitty.min.js': 'fitty.js',
-    'node_modules/@fortawesome/fontawesome-free/sprites': 'static/sprites',
   })
 
   // Config!
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './_pages/**/*.html',
+    './_site/**/*.html',
+  ],
+  darkMode: false,
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Barlow Condensed', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
