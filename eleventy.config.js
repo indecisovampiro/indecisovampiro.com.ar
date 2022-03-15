ecc63ba9d6f6e5bbdce63940f8e95bc021f4c54e
Sobrino Julian <sobrinojulian@protonmail.com>
2022-03-15 16:46:50 -0300

    *

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,54 @@
 const yaml = require('js-yaml')
-const { readableDate, renderUsingMarkdown, renderAsText } = require('./src/filters')
+const fs = require('fs')
+const MarkdownIt = require('markdown-it')
+const { DateTime } = require('luxon')
+const { convert } = require('html-to-text')
+const htmlmin = require('html-minifier')
+
+const OUTPUT_DIRECTORY = 'public'
+
+// Filters
+function readableDate(dateObj) {
+  return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('dd LLL yyyy')
+}
+
+function renderUsingMarkdown(content) {
+  const md = new MarkdownIt()
+  return md.render(content)
+}
+
+function renderAsText(content) {
+  return convert(content, { ignoreHref: true }).replace(/"/g, "'")
+}
+
+// Transformers
+function minifiedHTML(content, outputPath) {
+  // https://www.11ty.dev/docs/config/#transforms-example-minify-html-output
+  // Eleventy 1.0+: use this.inputPath and this.outputPath instead
+  if (outputPath && outputPath.endsWith('.html')) {
+    let minified = htmlmin.minify(content, {
+      useShortDoctype: true,
+      removeComments: true,
+      collapseWhitespace: true,
+    })
+    return minified
+  }
+  return content
+}
+
+function support404() {
+  return {
+    callbacks: {
+      ready: function (err, bs) {
+        bs.addMiddleware('*', (req, res) => {
+          res.writeHead(404, { 'Content-Type': 'text/html; charset=UTF-8' })
+          res.write(fs.readFileSync(`${OUTPUT_DIRECTORY}/404.html`))
+          res.end()
+        })
+      },
+    },
+  }
+}
 
 module.exports = function (config) {
   // Copy
@@ -9,12 +58,12 @@ module.exports = function (config) {
     'static/sprite.svg': 'static/sprite.svg',
     'static/fonts': 'static/fonts',
     'static/images': 'static/images',
-    'src/router.js': 'router.js',
-    'node_modules/alpinejs/dist/cdn.min.js': 'alpine.js'
+    'static/router.js': 'router.js',
+    'node_modules/alpinejs/dist/cdn.min.js': 'alpine.js',
   })
 
   // Plugins
-  config.addDataExtension('yaml', (contents) => yaml.load(contents))
+  config.addDataExtension('yaml', contents => yaml.load(contents))
 
   // Filters
   config.addFilter('readableDate', readableDate)
@@ -24,18 +73,18 @@ module.exports = function (config) {
   // Conditional configs
   const isProduction = process.env.NODE_ENV === 'production'
   if (isProduction) {
-    config.addTransform('minifyHtml', require('./src/minifyHtml'))
+    config.addTransform('minifyHtml', minifiedHTML)
   } else {
-    config.setBrowserSyncConfig(require('./src/browsersyncConfig'))
+    config.setBrowserSyncConfig(support404())
   }
 
   return {
     dir: {
       input: 'pages',
-      output: 'public',
+      output: OUTPUT_DIRECTORY,
       includes: '../includes',
       layouts: '../layouts',
-      data: '../data'
-    }
+      data: '../data',
+    },
   }
 }
