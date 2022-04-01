899f9032ae4afab96898f42ee8715b3aa56169aa
Sobrino Julian <sobrinojulian@protonmail.com>
2022-04-01 03:08:17 -0300

    refix

--- static/router.js
+++ static/router.js
@@ -1,5 +1,5 @@
-(function () {
-  'use strict';
+;(function () {
+  'use strict'
   /*https://github.com/terabaud/eleventy-mini-spa*/
   /**
    * Load content into page without a whole page reload
@@ -7,32 +7,42 @@
    * @param {boolean} pushState whether to call history.pushState or not
    */
   function load(href, pushState) {
-    const container = $('main');
-    const xhr = new XMLHttpRequest();
+    const container = $('main')
+    const xhr = new XMLHttpRequest()
     xhr.onload = function () {
-      const d = xhr.responseXML;
-      const dTitle = d.title || '';
-      const dContainer = $('main', d);
-      container.innerHTML = (dContainer && dContainer.innerHTML) || '';
-      document.title = dTitle;
+      const d = xhr.responseXML
+      const dTitle = d.title || ''
+      const dContainer = $('main', d)
+      container.innerHTML = (dContainer && dContainer.innerHTML) || ''
+      document.title = dTitle
       if (pushState) {
-        history.pushState({}, dTitle, href);
+        history.pushState({}, dTitle, href)
       }
-      container.focus();
-      window.scrollTo(0, 0);
-    };
+      container.focus()
+      window.scrollTo(0, 0)
+
+      var flkty = new Flickity('.carousel', {
+        autoPlay: 7000,
+        pauseAutoPlayOnHover: false,
+        wrapAround: true,
+        draggable: false,
+        prevNextButtons: false,
+        pageDots: false,
+      })
+      flkty.resize()
+    }
     xhr.onerror = function () {
       // fallback to normal link behaviour
-      document.location.href = href;
-      return;
-    };
-    xhr.open('GET', href);
-    xhr.responseType = 'document';
-    xhr.send();
+      document.location.href = href
+      return
+    }
+    xhr.open('GET', href)
+    xhr.responseType = 'document'
+    xhr.send()
   }
 
   function $(sel, con) {
-    return (con || document).querySelector(sel);
+    return (con || document).querySelector(sel)
   }
 
   /**
@@ -45,36 +55,32 @@
   function findAnchorTag(el, maxNests = 3) {
     for (let i = maxNests; el && i > 0; --i, el = el.parentNode) {
       if (el.nodeName === 'A') {
-        return el;
+        return el
       }
     }
-    return null;
+    return null
   }
 
   window.addEventListener('click', function (evt) {
-    let baseUrl = $('meta[name="x-base-url"]')?.getAttribute('content') || '/';
-    const el = findAnchorTag(evt.target);
-    const href = el?.getAttribute('href');
+    let baseUrl = $('meta[name="x-base-url"]')?.getAttribute('content') || '/'
+    const el = findAnchorTag(evt.target)
+    const href = el?.getAttribute('href')
     if (el && href) {
-      if (
-        href.startsWith('#') ||
-        el.getAttribute('target') === '_blank' ||
-        /\.\w+$/.test(href)
-      ) {
+      if (href.startsWith('#') || el.getAttribute('target') === '_blank' || /\.\w+$/.test(href)) {
         // eleventy urls in this configuration do not have extensions like .html
         // if they have, or if target _blank is set, or they are a hash link,
         // then do nothing.
-        return;
+        return
       }
       // if the URL starts with the base url, do the SPA handling
       if (href.startsWith(baseUrl)) {
-        evt.preventDefault();
-        load(href, true);
+        evt.preventDefault()
+        load(href, true)
       }
     }
-  });
+  })
 
   window.addEventListener('popstate', function (e) {
-    load(document.location.pathname, false);
-  });
-})();
+    load(document.location.pathname, false)
+  })
+})()
